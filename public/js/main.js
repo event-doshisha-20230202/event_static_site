b9a2e798a3942d66f5249a5799ebb9616f7ab49d
hirotoinomoto <hiroto_inomoto@demia.co.jp>
2023-02-10 04:26:07 +0900

    0202renew

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
-    window.alert("これはアラートです。");
+    document.getElementById("title").style.color = "rgb(50,180,200)"
+    document.getElementById("title").style.fontFamily = "courier"
   });
 });
